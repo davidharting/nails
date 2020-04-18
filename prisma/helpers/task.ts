33d09bbf49d8bee1864c8309c3e43df4baf658f0
David Harting <davideharting@gmail.com>
2020-04-18 11:10:07 -0400

    comment on toProps approach

--- prisma/helpers/task.ts
+++ prisma/helpers/task.ts
@@ -2,6 +2,9 @@ import { Task } from "@prisma/client";
 
 class TaskHelper {
   // Probably rename "serializable"?
+  // I would like to solve this at a more generic level
+  // Some kind of mapper that can take any prisma object and create a prop-friendly version
+  // through more generic introspection rather than field-for-field mapping
   static toProps(task: Task) {
     return {
       ...task,
