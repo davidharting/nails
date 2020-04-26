ceb74ee76fdc590fecc1142ba06660d3a749288d
David Harting <davideharting@gmail.com>
2020-04-26 12:39:26 -0400

    i fixed that

--- next.config.js
+++ next.config.js
@@ -2,7 +2,4 @@ if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
 
-// This logs a warning to export an empty config
-// However, I really just needed a place to hook into
-// the next lifecycle and require dotenv 🤷🏼‍♀️
 module.exports = (phase, { defaultConfig }) => defaultConfig;
